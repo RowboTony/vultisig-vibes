3d0ad80270634d9294392e9efb291641e0e53149
RowboTony <tgroff@gmail.com>
2025-04-04 23:54:12 -0500

    oops, forgot an hr

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -111,6 +111,7 @@ export default function Layout({ children }) {
         {isHomePage ? (
           <>
             <Hero />
+            <GradientDivider />
             <FeatureGrid />
             <GradientDivider />
             <RespawnRestore />
