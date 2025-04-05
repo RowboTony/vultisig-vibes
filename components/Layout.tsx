d9e495eb892896510974f20c1a2e63692ad524fb
RowboTony <tgroff@gmail.com>
2025-04-04 21:06:19 -0500

    Respawn & Restore locked-in!

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -44,6 +44,8 @@ export default function Layout({ children }) {
 
             <GradientDivider />
             <RespawnRestore />
+            <GradientDivider />
+
             {/* <FinalProofCredibility /> */}
           </>
         ) : (
